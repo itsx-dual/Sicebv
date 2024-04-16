9e3aae9d4615dc2912ea257fbc6033fe2a7c151e
EduardoROFO <edunar75@gmail.com>
2024-04-16 11:34:32 -0600

    correccion v4

--- Cebv/features/prendas/domain/PrendasVestirNetwork.cs
+++ Cebv/features/prendas/domain/PrendasVestirNetwork.cs
@@ -19,39 +19,5 @@ public class PrendasVestirNetwork
         return dic;
     }
     
-    public static async Task<Dictionary<string, Grupo_pertenencias>> GetGrupo_pertenencias()
-    {
-        using HttpResponseMessage response = await Client.GetAsync("api/catalogos/Grupo_pertenencias");
-        var jsonResponse = await response.Content.ReadAsStringAsync();
-        List<Grupo_pertenencias> Grupopertenencia = JsonSerializer.Deserialize<List<Grupo_pertenencias>>(jsonResponse);
-        Dictionary<string, Grupo_pertenencias> dic = new Dictionary<string, Grupo_pertenencias>();
-        return dic;
-    }
     
-    public static async Task<Dictionary<string, Material>> GetGrupo_pertenencias()
-    {
-        using HttpResponseMessage response = await Client.GetAsync("api/catalogos/Grupo_pertenencias");
-        var jsonResponse = await response.Content.ReadAsStringAsync();
-        List<Material> material = JsonSerializer.Deserialize<List<Material>>(jsonResponse);
-        Dictionary<string, Material> dic = new Dictionary<string, Material>();
-        return dic;
-    }
-    
-    public static async Task<Dictionary<string, Pertenencias>> GetGrupo_pertenencias()
-    {
-        using HttpResponseMessage response = await Client.GetAsync("api/catalogos/Grupo_pertenencias");
-        var jsonResponse = await response.Content.ReadAsStringAsync();
-        List<Pertenencias> pertenencias = JsonSerializer.Deserialize<List<Pertenencias>>(jsonResponse);
-        Dictionary<string, Pertenencias> dic = new Dictionary<string, Pertenencias>();
-        return dic;
-    }
-    
-    public static async Task<Dictionary<string, Prendas_Vestir_Pertenencias>> GetPVP()
-    {
-        using HttpResponseMessage response = await Client.GetAsync("api/catalogos/Grupo_pertenencias");
-        var jsonResponse = await response.Content.ReadAsStringAsync();
-        List<Prendas_Vestir_Pertenencias> pvp = JsonSerializer.Deserialize<List<Prendas_Vestir_Pertenencias>>(jsonResponse);
-        Dictionary<string, Prendas_Vestir_Pertenencias> dic = new Dictionary<string, Prendas_Vestir_Pertenencias>();
-        return dic;
-    }
 }
