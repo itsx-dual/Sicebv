410b53230390f1769d06e8d3c3b7d8f29259a4ba
Peloponeso31 <tanilizquierdo@proton.me>
2024-05-03 17:01:13 -0600

    Se resuelve problema al poner contenido dentro del formulario.

--- Cebv/features/formulario_cebv/presentation/FormularioCebvPage.xaml.cs
+++ Cebv/features/formulario_cebv/presentation/FormularioCebvPage.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using Cebv.features.formulario_cebv.datos_del_reporte.presentation;
 
 namespace Cebv.features.formulario_cebv.presentation;
 
@@ -7,5 +9,10 @@ public partial class FormularioCebvPage : Page
     public FormularioCebvPage()
     {
         InitializeComponent();
+    }
+
+    private void FormularioCebvPage_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        FormularioNavigationView.Navigate(typeof(DatosReportePage));
     }
 }
