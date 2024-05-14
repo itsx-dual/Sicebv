b2eca8be08fa49bae23822e759bf30587b3f5e31
Jonatan Luna Franco <157180405+untaljnn@users.noreply.github.com>
2024-05-14 11:55:52 -0600

    merge pages

---
+++ Cebv/features/formulario_cebv/control_ogpi/presentation/ControlOgpiPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Cebv.features.formulario_cebv.control_ogpi.presentation;
+
+public partial class ControlOgpiPage : Page
+{
+    public ControlOgpiPage()
+    {
+        InitializeComponent();
+    }
+}
