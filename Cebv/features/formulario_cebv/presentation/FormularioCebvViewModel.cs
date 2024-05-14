226b41d96befa36a756b656f641f3fd0f90766db
Gau Olarte Martínez <tucorreo@hola>
2024-05-14 11:53:58 -0600

    update

--- Cebv/features/formulario_cebv/presentation/FormularioCebvViewModel.cs
+++ Cebv/features/formulario_cebv/presentation/FormularioCebvViewModel.cs
@@ -7,16 +7,24 @@ using CommunityToolkit.Mvvm.Messaging;
 namespace Cebv.features.formulario_cebv.presentation;
 
 public partial class FormularioCebvViewModel : ObservableObject, IRecipient<AddReporteMessage>,
-    IRecipient<NombreCompletoMessage>
+    IRecipient<NombreCompletoMessage>, IRecipient<GuardarBorradorMessage>
 {
     private readonly IFormularioService _formularioService;
     [ObservableProperty] private string _nombreCompleto = string.Empty;
+    [ObservableProperty] private bool _puedeGuardar;
+    
+    /**
+     * _puedeGuardar;
+     */
+    
+    
 
     public FormularioCebvViewModel(IFormularioService formularioService)
     {
         _formularioService = formularioService;
         WeakReferenceMessenger.Default.Register<NombreCompletoMessage>(this);
         WeakReferenceMessenger.Default.Register<AddReporteMessage>(this);
+        WeakReferenceMessenger.Default.Register<GuardarBorradorMessage>(this);
     }
 
     public void Receive(AddReporteMessage message)
@@ -27,5 +35,10 @@ public partial class FormularioCebvViewModel : ObservableObject, IRecipient<AddR
     public void Receive(NombreCompletoMessage message)
     {
         NombreCompleto = message.Value;
+    }
+
+    public void Receive(GuardarBorradorMessage message)
+    {
+        PuedeGuardar = message.Value;
     }
 }
