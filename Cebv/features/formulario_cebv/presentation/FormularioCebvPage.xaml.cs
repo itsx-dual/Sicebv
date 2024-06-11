62928c92621935cc7f3b2e14fcd0bf4dfab8654a
Peloponeso31 <tanilizquierdo@proton.me>
2024-06-11 16:23:32 -0600

    Servicio expuesto de navegacion

--- Cebv/features/formulario_cebv/presentation/FormularioCebvPage.xaml.cs
+++ Cebv/features/formulario_cebv/presentation/FormularioCebvPage.xaml.cs
@@ -1,18 +1,21 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Cebv.core.util.navigation;
 using Cebv.features.formulario_cebv.datos_del_reporte.presentation;
 using Microsoft.Extensions.DependencyInjection;
 using Wpf.Ui.Controls;
 
 namespace Cebv.features.formulario_cebv.presentation;
 
-public partial class FormularioCebvPage : Page
+public partial class FormularioCebvPage
 {
     public FormularioCebvPage()
     {
         InitializeComponent();
         DataContext = App.Current.Services.GetService<FormularioCebvViewModel>();
+        var navigationService = App.Current.Services.GetService<IFormularioCebvNavigationService>();
+        navigationService.SetNavigationControl(FormularioNavigationView);
     }
 
     private void FormularioCebvPage_OnLoaded(object sender, RoutedEventArgs e)
