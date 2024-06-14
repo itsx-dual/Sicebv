d65497ccbd7a04ba72a7ee762804f4e1e422a2fd
Jonatan Luna Franco <157180405+untaljnn@users.noreply.github.com>
2024-06-14 01:34:37 -0600

    update datos de localizacion module

--- Cebv/features/formulario_cebv/datos_de_localizacion/presentation/DatosLocalizacionViewModel.cs
+++ Cebv/features/formulario_cebv/datos_de_localizacion/presentation/DatosLocalizacionViewModel.cs
@@ -1,8 +1,11 @@
 using System.Collections.ObjectModel;
+using System.IO;
 using Cebv.core.domain;
 using Cebv.core.modules.ubicacion.data;
 using Cebv.core.modules.ubicacion.domain;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
 
 namespace Cebv.features.formulario_cebv.datos_de_localizacion.presentation;
 
@@ -26,7 +29,95 @@ public partial class DatosLocalizacionViewModel : ObservableObject
     private async void CargarCatalogos() =>
         Estados = await UbicacionNetwork.GetEstados();
     
-
     async partial void OnEstadoChanged(Estado value) =>
         Municipios = await UbicacionNetwork.GetMuncipios(value.Id);
+    
+    
+    /**
+     * Path de las imagenes seleccionadas
+     */
+    [ObservableProperty]
+    private string[]? _openedFilePathUno = [];
+    
+    [RelayCommand]
+    private void OnOpenFileUno()
+    {
+        OpenFileDialog openFileDialog =
+            new()
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                Multiselect = true
+            };
+
+        if (openFileDialog.ShowDialog() != true)
+        {
+            return;
+        }
+
+        if (!File.Exists(openFileDialog.FileName))
+        {
+            return;
+        }
+
+        OpenedFilePathUno = openFileDialog.FileNames;
+    }
+    
+    
+    /**
+     * Path de las imagenes seleccionadas
+     */
+    [ObservableProperty]
+    private string[]? _openedFilePathDos = [];
+    
+    [RelayCommand]
+    private void OnOpenFileDos()
+    {
+        OpenFileDialog openFileDialog =
+            new()
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                Multiselect = true
+            };
+
+        if (openFileDialog.ShowDialog() != true)
+        {
+            return;
+        }
+
+        if (!File.Exists(openFileDialog.FileName))
+        {
+            return;
+        }
+
+        OpenedFilePathDos = openFileDialog.FileNames;
+    }
+    
+    /**
+     * Path de las imagenes seleccionadas
+     */
+    [ObservableProperty]
+    private string[]? _openedFilePathTres = [];
+    
+    [RelayCommand]
+    private void OnOpenFileTres()
+    {
+        OpenFileDialog openFileDialog =
+            new()
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                Multiselect = true
+            };
+
+        if (openFileDialog.ShowDialog() != true)
+        {
+            return;
+        }
+
+        if (!File.Exists(openFileDialog.FileName))
+        {
+            return;
+        }
+
+        OpenedFilePathTres = openFileDialog.FileNames;
+    }
 }
